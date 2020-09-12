bc7f74baaff32212b738c395de2a66e5e1eceb69
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-09-12 17:18:05 +0300

    Working with Props

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
     <div className="App">
       <h1>Hello 1</h1>
       <h1>Hello 2</h1>
-      <Person/>
-      <Person/>
-      <Person/>
+      <Person name='Max' age='22' />
+      <Person name='Dan' age='33' >My hobbies: Cycling</Person>
+      <Person name='Jenny' age='20' />
     </div>
   );
 }
