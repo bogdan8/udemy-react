da6aacfb6a67bdea3010a2d7a0f92726eb3c7c44
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-09-12 16:45:38 +0300

    Understanding JSX

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <p>Hello</p>
-    </div>
-  );
+  return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hello i am hrere'));
 }
 
 export default App;
