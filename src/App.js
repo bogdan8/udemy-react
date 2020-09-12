0b98a9ef88024fd72774ae5f94145ae0736baaaa
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-09-12 17:21:10 +0300

    Understanding the "children" Prop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import './App.css';
-import Person from './Person/Person';
+import React from 'react'
+import './App.css'
+import Person from './Person/Person'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello 1</h1>
-      <h1>Hello 2</h1>
-      <Person name='Max' age='22' />
-      <Person name='Dan' age='33' >My hobbies: Cycling</Person>
-      <Person name='Jenny' age='20' />
-    </div>
-  );
+  return <div className="App">
+    <h1>Hello 1</h1>
+    <h1>Hello 2</h1>
+    <Person name='Max' age='22' />
+    <Person name='Dan' age='33' >My hobbies: Cycling</Person>
+    <Person name='Jenny' age='20' />
+  </div>
 }
 
-export default App;
+export default App
