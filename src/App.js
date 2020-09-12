051501fc485e37bf2833c26b6588683b2d59520b
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-09-12 16:57:10 +0300

    Creating a Functional Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
-    <div class="App">
+    <div className="App">
       <h1>Hello 1</h1>
+      <h1>Hello 2</h1>
+      <Person/>
     </div>
-    <h1>Hello 2</h1>
   );
 }
 
