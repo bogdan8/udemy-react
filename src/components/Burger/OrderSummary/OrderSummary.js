d3f36b4ea0ff1048fa8fcde107ee7b3fc8da4301
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-09-26 13:24:52 +0300

    Adding a Custom Button Component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -13,7 +13,9 @@ const orderSummary = (props) => {
     <ul>
       { ingredientSummary }
     </ul>
-    <p>Continue to checkout?</p>
+    <p>Continue to Checkout?</p>
+    <button>CANCEL</button>
+    <button>CONTINUE</button>
   </>
 }
 
---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import classes from './Button.module.css'
+
+const button = (props) => <button className={ [classes.Button, classes[props.btnType]].join(' ') } onClick={ props.chlicked }>{ props.children }</button>
+
+export default button
