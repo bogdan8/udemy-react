5e591af8a8ed1cd814442ae62b3779424acf6f7e
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-09-28 12:24:56 +0300

    Revert "Making a Post Selectable"
    This reverts commit 142759eb1718aa2add56c8c0ad191c65606c0e64.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,8 +8,7 @@ import './Blog.css'
 
 class Blog extends Component {
   state = {
-    posts: [],
-    selectedPostId: null
+    posts: []
   }
 
   componentDidMount () {
@@ -25,22 +24,18 @@ class Blog extends Component {
     })
   }
 
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id })
-  }
-
   render () {
     const posts = this.state.posts.map(post => {
-      return <Post key={ post.id } title={ post.title } author={ post.author } clicked={ () => this.postSelectedHandler(post.id) } />
+      return <Post key={ post.id } title={ post.title } author={ post.author } />
     })
 
     return (
       <div>
-        <section className='Posts'>
+        <section className="Posts">
           { posts }
         </section>
         <section>
-          <FullPost id={ this.state.selectedPostId } />
+          <FullPost />
         </section>
         <section>
           <NewPost />
