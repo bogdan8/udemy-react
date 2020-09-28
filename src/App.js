339df98409b0eac91fa62d84462881b9401b3b4a
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-09-28 12:24:56 +0300

    Revert "Reaching out to the Web (Http / Ajax)"
    This reverts commit 818bcc36a0c1ec7f2e9cb0763a0c972ebaaf7dfb.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Blog from './containers/Blog/Blog';
+import Layout from './hoc/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Blog />
-      </div>
-    );
+  render () {
+    return <div>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
+    </div>
   }
 }
 
-export default App;
+export default App
