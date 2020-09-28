38cdb6ab33f4d6a302a66923782fa93baf1851eb
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-09-28 12:24:56 +0300

    Revert "Handling Errors Locally"
    This reverts commit 49e75fe1b901c296f2adbff854dbb9d90110b5b5.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,8 +9,7 @@ import './Blog.css'
 class Blog extends Component {
   state = {
     posts: [],
-    selectedPostId: null,
-    error: false
+    selectedPostId: null
   }
 
   componentDidMount () {
@@ -23,8 +22,6 @@ class Blog extends Component {
         }
       })
       this.setState({ posts: updatedPosts })
-    }).catch(error => {
-      this.setState({ error: true })
     })
   }
 
@@ -33,13 +30,9 @@ class Blog extends Component {
   }
 
   render () {
-    let posts = <p style={ { textAlign: 'center' } }>Someting went wrong</p>
-
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return <Post key={ post.id } title={ post.title } author={ post.author } clicked={ () => this.postSelectedHandler(post.id) } />
-      })
-    }
+    const posts = this.state.posts.map(post => {
+      return <Post key={ post.id } title={ post.title } author={ post.author } clicked={ () => this.postSelectedHandler(post.id) } />
+    })
 
     return (
       <div>
