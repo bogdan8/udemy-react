fba4a4e51d58cc3ea06e627c5ec1cf4ddff06c7e
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-09-28 12:24:56 +0300

    Revert "Creating and Using Axios Instances"
    This reverts commit 44de5e28f6f57c36ca255c48150e1d597fca58ae.

--- src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-let instance = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com'
-})
-
-instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN'
-
-export default instance
