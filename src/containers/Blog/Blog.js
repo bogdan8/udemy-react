637476a24142814d336a0cd496dfdd1dd9b1965b
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-09-28 12:24:56 +0300

    Revert "Creating a Http Request to GET Data"
    This reverts commit f0f2b7f526fd0a6f86764c5b8f5eaa8b260bb23d.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import axios from 'axios'
 
 import Post from '../../components/Post/Post'
 import FullPost from '../../components/FullPost/FullPost'
@@ -7,12 +6,6 @@ import NewPost from '../../components/NewPost/NewPost'
 import './Blog.css'
 
 class Blog extends Component {
-  componentDidMount () {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-      console.log(response)
-    })
-  }
-
   render () {
     return (
       <div>
