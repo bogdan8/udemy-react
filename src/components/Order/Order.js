b498be1df647fc787d00b4a6a465386d650980a0
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-10-01 12:21:55 +0300

    Implementing Navigation Links

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import classes from './Order'
+import classes from './Order.module.css'
 
 const order = (props) => <div className={ classes.Order }>
   <p>Ingredients: Salad(1)</p>
