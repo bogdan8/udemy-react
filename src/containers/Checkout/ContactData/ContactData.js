e210f7e0e20aa84a42ba38252a63bf86019de5a8
Bohdan Lytvyn <bogdanlitvin8@gmail.com>
2020-10-01 15:48:03 +0300

    Setting Up a JS Config for the Form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -9,11 +9,57 @@ import classes from './ContactData.module.css'
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Zip Code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your E-Mail'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'chipest', displayValue: 'Chipest' }
+          ]
+        },
+        value: ''
+      }
     },
     loading: false
   }
@@ -25,17 +71,7 @@ class ContactData extends Component {
 
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.totalPrice,
-      customer: {
-        name: 'Bob',
-        address: {
-          street: 'Test',
-          zipCode: '76000',
-          country: 'Ukraine'
-        },
-        email: 'test@gmail.com'
-      },
-      deliveryMethod: 'faster'
+      price: this.props.totalPrice
     }
 
     axios.post('/orders.json', order).then(response => {
@@ -48,6 +84,7 @@ class ContactData extends Component {
 
   render () {
     let form = <form>
+      <Input elementType='...' elementConfig='...' />
       <Input inputtype='input' type='text' name='name' placeholder='Your name' />
       <Input inputtype='input' type='email' name='email' placeholder='Your email' />
       <Input inputtype='input' type='text' name='street' placeholder='Street' />
