a92405e483339c7c8d11bd7c40551fc6376df114
bogdan8 <bogdanlitvin8@gmail.com>
2020-10-02 15:10:20 +0300

    Dispatching Actions from within the Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Counter from './containers/Counter/Counter';
-import './App.css';
+import Counter from './containers/Counter/Counter'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -9,8 +9,8 @@ class App extends Component {
       <div className="App">
        <Counter />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
