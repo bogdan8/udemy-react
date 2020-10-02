f3800036026cc83b59be856c0576e592cac418ba
bogdan8 <bogdanlitvin8@gmail.com>
2020-10-02 11:56:11 +0300

    Setting Up Reducer and Store

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
+import React, { Component } from 'react';
 
-import Layout from './hoc/Layout/Layout'
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
-import Checkout from './containers/Checkout/Checkout'
-import Orders from './containers/Orders/Orders'
+import Counter from './containers/Counter/Counter';
+import './App.css';
 
 class App extends Component {
-  render () {
-    return <div>
-      <Layout>
-        <Route path='/checkout' component={ Checkout } />
-        <Route path='/orders' component={ Orders } />
-        <Route path='/' exact component={ BurgerBuilder } />
-      </Layout>
-    </div>
+  render() {
+    return (
+      <div className="App">
+       <Counter />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
