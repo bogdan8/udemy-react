c87ea3f39660684d4641322acf65a64524e3ee49
bogdan8 <bogdanlitvin8@gmail.com>
2020-10-04 10:54:52 +0300

    Working on Order Actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -93,8 +93,7 @@ class ContactData extends Component {
         validation: {}
       }
     },
-    formIsValid: false,
-    loading: false
+    formIsValid: false
   }
 
   orderHandler = (e) => {
@@ -176,7 +175,7 @@ class ContactData extends Component {
       <Button btnType='Success' disabled={ !this.state.formIsValid }>Order</Button>
     </form>
 
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />
     }
 
@@ -191,13 +190,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData)) 
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData)) 
   }
 }
 
