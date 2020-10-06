24274406c07a6bea8c8548c10edc38ac414d8761
bogdan8 <bogdanlitvin8@gmail.com>
2020-10-06 17:19:06 +0300

    Loading CSS Files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const autoprefixer = require('autoprefixer')
 
 module.export = {
   mode: 'development',
@@ -15,6 +16,27 @@ module.export = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: '/node_modules'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                localIdentName: '[name]__[local]__[hash:base64:5]'
+              }
+            }
+          },
+          { loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [autoprefixer]
+            }
+          }
+        ]
       }
     ]
   }
