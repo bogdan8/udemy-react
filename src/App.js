ba9058827cf792c6ff8ef660eb273bd0b1ef0da2
bogdan8 <bogdanlitvin8@gmail.com>
2020-10-07 10:35:10 +0300

    Transition Events

--- src/App.js
+++ src/App.js
@@ -26,8 +26,18 @@ class App extends Component {
         <h1>React Animations</h1>
         <button className='Button' onClick={ () => this.setState(prevState => ({ showBlock: !prevState.showBlock })) }>Toggle</button>
         <br />
-        <Transition in={ this.state.showBlock } timeout={ 1000 } mountOnEnter unmountOnExit>
-          { state => <div style={ { backgroundColor: 'red', width: 100, height: 100, margin: 'auto', transition: 'opacity 1s ease-out', opacity: state == 'exiting' ? 0 : 1 } }></div> }
+        <Transition
+          in={ this.state.showBlock }
+          timeout={ 1000 }
+          mountOnEnter unmountOnExit
+          onEnter={ () => console.log('enter') }
+          onEntering={ () => console.log('entering') }
+          onEntered={ () => console.log('entered') }
+          onExit={ () => console.log('enter') }
+          onExiting={ () => console.log('entering') }
+          onExited={ () => console.log('entered') }
+        >
+          { state => <div style={ { backgroundColor: 'red', width: 100, height: 100, margin: 'auto', transition: 'opacity 1s ease-out', opacity: state === 'exiting' ? 0 : 1 } }></div> }
         </Transition>
         <Modal show={ this.state.modalIsOpen } closed={this.closeModal}/>
         { this.state.modalIsOpen ? <Backdrop show={ this.state.modalIsOpen } /> : null }
