abfc9f43dde27cfdd2f063eea88648986d3322f0
bogdan8 <bogdanlitvin8@gmail.com>
2020-10-07 10:26:27 +0300

    Wrapping the Transition Component

--- src/App.js
+++ src/App.js
@@ -29,11 +29,7 @@ class App extends Component {
         <Transition in={ this.state.showBlock } timeout={ 1000 } mountOnEnter unmountOnExit>
           { state => <div style={ { backgroundColor: 'red', width: 100, height: 100, margin: 'auto', transition: 'opacity 1s ease-out', opacity: state == 'exiting' ? 0 : 1 } }></div> }
         </Transition>
-        <Transition in={ this.state.modalIsOpen } timeout={ 300 } mountOnEnter unmountOnExit>
-          {
-            state => <Modal show={ state } closed={this.closeModal}/>
-          }
-        </Transition>
+        <Modal show={ this.state.modalIsOpen } closed={this.closeModal}/>
         { this.state.modalIsOpen ? <Backdrop show={ this.state.modalIsOpen } /> : null }
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
