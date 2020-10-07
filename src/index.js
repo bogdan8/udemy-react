5ddcf0419d338ecd7670d33a6460315d3c0845ae
bogdan8 <bogdanlitvin8@gmail.com>
2020-10-07 16:03:51 +0300

    Diving Deeper into Sagas

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 import createSagaMiddleware from 'redux-saga'
 
 import './index.css'
@@ -23,7 +22,7 @@ const rootReducer = combineReducers({
 const sagaMiddleware = createSagaMiddleware()
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware))) 
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware))) 
 
 sagaMiddleware.run(watchAuth)
 sagaMiddleware.run(watchBurgerBuilder)
