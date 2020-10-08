01db96352ac83f2ae17bcab26c0f08a3925a9b12
bogdan8 <bogdanlitvin8@gmail.com>
2020-10-08 11:53:20 +0300

    Routing with React.lazy()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component, Suspense } from 'react'
 import { Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import asyncComponent from './hoc/asyncComponent/asyncComponent'
 
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Logout from './containers/Auth/Logout/Logout'
 import * as actions from './store/actions/'
 
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
   return import('./containers/Checkout/Checkout')
 })
 
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
   return import('./containers/Orders/Orders')
 })
 
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
   return import('./containers/Auth/Auth')
 })
 
@@ -27,7 +26,7 @@ class App extends Component {
 
   render () {
     let routes = <>
-      <Route path='/auth' component={ asyncAuth } />
+      <Route path='/auth' render={ () => <Auth /> } />
       <Route path='/' exact component={ BurgerBuilder } />
       <Redirect to='/' />
     </>
@@ -35,8 +34,8 @@ class App extends Component {
 
     if (this.props.isAuthenticated) {
       routes = <>
-        <Route path='/checkout' component={ asyncCheckout } />
-        <Route path='/orders' component={ asyncOrders } />
+        <Route path='/checkout' render={ () => <Checkout /> } />
+        <Route path='/orders' render={ () => <Orders /> } />
         <Route path='/logout' component={ Logout } />
         <Route path='/' exact component={ BurgerBuilder } />
         <Redirect to='/' />
@@ -45,7 +44,7 @@ class App extends Component {
 
     return <div>
       <Layout>
-        { routes }
+        <Suspense fallback={ <p>loading</p> }>{ routes }</Suspense>
       </Layout>
     </div>
   }
