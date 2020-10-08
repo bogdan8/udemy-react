416089d5228ab1b7959ac1bf2dfbd3cb10c35629
bogdan8 <bogdanlitvin8@gmail.com>
2020-10-08 09:46:24 +0300

    Understanding useReducer()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,12 +1,27 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useReducer, useState, useEffect, useCallback } from 'react'
 
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
 import ErrorModal from '../UI/ErrorModal'
 import Search from './Search'
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch(action.type) {
+    case 'SET':
+      return action.ingredients
+    case 'ADD':
+      return [ ...currentIngredients, action.ingredient ]
+    case 'DELETE':
+      return currentIngredients.filter(ing => ing.id !== action.id)
+    default:
+      throw new Error('Should not get there')
+  }
+}
+
 const Ingredients = () => {
-  const [ ingredients, setIngredients ] = useState([])
+  const [ ingredients, dispatch ] = useReducer(ingredientReducer, [])
+
+  // const [ ingredients, setIngredients ] = useState([])
   const [ isLoading, setIsLoading ] = useState(false)
   const [ error, setError ] = useState()
 
@@ -22,7 +37,10 @@ const Ingredients = () => {
           amount: responseData[key].amount
         })
       }
-      setIngredients(loadedIngredients)
+      dispatch({
+        type: 'SET',
+        ingredients: loadedIngredients
+      })
     })
   }, [])
 
@@ -42,12 +60,20 @@ const Ingredients = () => {
 
       return response.json()
     }).then(responseData => {
-      setIngredients(prevIngredients => [ ...prevIngredients, { id: responseData.name, ...ingredient } ])
+      // setIngredients(prevIngredients => [ ...prevIngredients, { id: responseData.name, ...ingredient } ])
+      dispatch({
+        type: 'ADD',
+        ingredient: { id: responseData.name, ...ingredient }
+      })
     })
   }
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setIngredients(filteredIngredients)
+    // setIngredients(filteredIngredients)
+    dispatch({
+      type: 'SET',
+      ingredients: filteredIngredients
+    })
   }, [])
 
   const removeIngredientHandler = (ingId) => {
@@ -57,7 +83,11 @@ const Ingredients = () => {
       method: 'DELETE'
     }).then(response => {
       setIsLoading(false)
-      setIngredients(prevIngredients => [ ...prevIngredients.filter( ingredient => ingredient.id !== ingId ) ])
+      // setIngredients(prevIngredients => [ ...prevIngredients.filter( ingredient => ingredient.id !== ingId ) ])
+      dispatch({
+        type: 'DELETE',
+        id: ingId
+      })
     }).catch(error => {
       setError(error.message)            
       setIsLoading(false)
